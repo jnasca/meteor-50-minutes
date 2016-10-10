a5c93c18180762e83fa5de0f8492baf9f1c5cbd0
jnasca <jnasca221@gmail.com>
2016-10-09 22:49:53 -1000

    Task 2: Tasklist setup

---
+++ app/tasklist.js
@@ -0,0 +1,18 @@
+/**
+ * Tasklist
+ * Created by Joseph on 10/9/2016.
+ */
+
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient){
+  Template.tasks.helpers({
+    tasks: function(){
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
